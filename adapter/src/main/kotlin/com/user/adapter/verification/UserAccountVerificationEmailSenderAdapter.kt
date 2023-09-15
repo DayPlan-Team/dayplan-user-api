60e470e6a3d0fcdcda9708c0840c2d0060f79d2a
gosekose <ksyn1611@naver.com>
2023-09-15 19:28:46 +0900

    [Feat] DPL-002: 유저 계정 검증 서비스 어댑터 초기화 - 계정 검증을 위한 방법, 상태 외부 연결

---
+++ adapter/src/main/kotlin/com/user/adapter/verification/UserAccountVerificationEmailSenderAdapter.kt
@@ -0,0 +1,14 @@
+package com.user.adapter.verification
+
+import com.user.application.port.UserAccountVerificationSenderPort
+import com.user.application.request.UserAccountVerificationStartRequest
+import com.user.application.response.UserAccountVerificationStartResponse
+import org.springframework.stereotype.Component
+
+//@Component
+//class UserAccountVerificationEmailSenderAdapter : UserAccountVerificationSenderPort {
+//    override fun sendVerification(request: UserAccountVerificationStartRequest): UserAccountVerificationStartResponse {
+//        TODO("Not yet implemented")
+//    }
+//
+//}
