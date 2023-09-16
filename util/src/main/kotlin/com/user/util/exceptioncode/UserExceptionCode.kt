737a59e65fbf0bcae4d07306a7ebd89fb84c460c
gosekose <ksyn1611@naver.com>
2023-09-16 19:48:01 +0900

    [Feat] DPL-002: 소셜 로그인 타입 추가 - 타입 및 예외 코드 추가

--- util/src/main/kotlin/com/user/util/exceptioncode/UserExceptionCode.kt
+++ util/src/main/kotlin/com/user/util/exceptioncode/UserExceptionCode.kt
@@ -9,5 +9,6 @@ enum class UserExceptionCode(
     USER_CREATION_INVALID_PASSWORD("400", "USR0001", "패스워드가 일치하지 않아요."),
     USER_CREATION_INVALID_PHONE_NUMBER("400", "USR0002", "잘못된 휴대폰 번호에요."),
     USER_CREATION_INVALID_EMAIL("400", "USR0003", "잘못된 이메일이에요."),
+    USER_SOCIAL_INVALID_TYPE("400", "USR0004", "로그인 방식이 잘못되었어요."),
 
 }
---
+++ util/src/main/kotlin/com/user/util/social/SocialType.kt
@@ -0,0 +1,19 @@
+package com.user.util.social
+
+import com.user.util.exception.UserException
+import com.user.util.exceptioncode.UserExceptionCode
+
+enum class SocialType(
+    val registrationId: String,
+) {
+    GOOGLE("google");
+
+    companion object {
+        fun matchSocialType(registrationId: String): SocialType {
+            return when (registrationId) {
+                GOOGLE.registrationId -> GOOGLE
+                else -> throw UserException(UserExceptionCode.USER_SOCIAL_INVALID_TYPE)
+            }
+        }
+    }
+}
