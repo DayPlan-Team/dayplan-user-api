9791069bbb57d1865ae1cefa10bd1f233ba8b65b
gosekose <ksyn1611@naver.com>
2023-09-19 02:17:27 +0900

    [Refac] DPL-014: 위치 도메인 리펙토링

--- domain/src/main/kotlin/com/user/domain/location/Location.kt
+++
@@ -1,6 +0,0 @@
-package com.user.domain.location
-
-data class Location(
-    val latitude: String,
-    val longitude: String,
-)
---
+++ domain/src/main/kotlin/com/user/domain/userlocation/Location.kt
@@ -0,0 +1,8 @@
+package com.user.domain.userlocation
+
+import com.user.domain.user.User
+
+data class Location(
+    val latitude: Double,
+    val longitude: Double,
+)
