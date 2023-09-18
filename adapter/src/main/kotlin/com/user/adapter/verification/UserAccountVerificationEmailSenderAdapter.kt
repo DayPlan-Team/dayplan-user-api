9e684aec4a3aaf81c79182fb65a4b6e77df49693
gosekose <ksyn1611@naver.com>
2023-09-19 02:01:18 +0900

    [Feat] DPL-010: 구글 인증으로, 유저 계정 검증 제거

--- adapter/src/main/kotlin/com/user/adapter/verification/UserAccountVerificationEmailSenderAdapter.kt
+++
@@ -1,9 +0,0 @@
-package com.user.adapter.verification
-
-//@Component
-//class UserAccountVerificationEmailSenderAdapter : UserAccountVerificationSenderPort {
-//    override fun sendVerification(request: UserAccountVerificationStartRequest): UserAccountVerificationStartResponse {
-//        TODO("Not yet implemented")
-//    }
-//
-//}
--- application/src/main/kotlin/com/user/application/request/UserAccountVerificationStartRequest.kt
+++
@@ -1,8 +0,0 @@
-package com.user.application.request
-
-import com.user.domain.share.AccountVerificationMeans
-
-data class UserAccountVerificationStartRequest(
-    val accountVerificationMeans: AccountVerificationMeans,
-    val account: String,
-)
