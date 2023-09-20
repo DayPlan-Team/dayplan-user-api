b853e9bb2ee436a2257c6bd69b8bcdadb193226a
gosekose <ksyn1611@naver.com>
2023-09-21 01:00:55 +0900

    [Feat] DPL-020: 유저 변경에 따른 약관 일부 수정

--- application/src/main/kotlin/com/user/application/port/out/TermsAgreementPort.kt
+++ application/src/main/kotlin/com/user/application/port/out/TermsAgreementPort.kt
@@ -2,12 +2,13 @@ package com.user.application.port.out
 
 import com.user.domain.terms.TermsAgreement
 import com.user.application.request.TermsAgreementRequest
+import com.user.domain.user.User
 import org.springframework.stereotype.Component
 
 @Component
 interface TermsAgreementPort {
 
-    fun findTermsAgreementsByUserId(userId: Long): List<TermsAgreement>
+    fun findTermsAgreementsByUserId(user: User): List<TermsAgreement>
 
-    fun upsertTermsAgreement(userId: Long, termsAgreementRequests: List<TermsAgreementRequest>)
+    fun upsertTermsAgreement(user: User, termsAgreementRequests: List<TermsAgreementRequest>)
 }
