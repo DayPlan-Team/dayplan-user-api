d557df32699ca3e8dd9cfd608f654805b72a3c07
gosekose <ksyn1611@naver.com>
2023-09-22 19:58:50 +0900

    [Feat] DPL-023: 지오코딩 유틸

--- util/src/main/kotlin/com/user/util/address/AddressUtil.kt
+++ util/src/main/kotlin/com/user/util/address/AddressUtil.kt
@@ -5,6 +5,14 @@ import org.springframework.stereotype.Component
 @Component
 object AddressUtil {
 
+    private val cityToCityNameMap = CityCode
+        .values()
+        .groupBy { it.koreanName }
+
+    private val districtToDistrictNameMap = DistrictCode
+        .values()
+        .groupBy { it.koreanName }
+
     val cityToDistrictMap = DistrictCode
         .values()
         .groupBy { it.city }
@@ -12,4 +20,17 @@ object AddressUtil {
     val codeToDistrictMap = DistrictCode
         .values()
         .groupBy { it.code }
+
+
+    fun transformToAddress(city: String, district: String): AddressCode {
+        val cityCode = cityToCityNameMap[city]?.get(0) ?: return AddressCode()
+        val districtCode = districtToDistrictNameMap[district]?.get(0) ?: return AddressCode()
+
+        return AddressCode(
+            city = city,
+            district = district,
+            cityCode = cityCode.code,
+            districtCode = districtCode.code
+        )
+    }
 }
