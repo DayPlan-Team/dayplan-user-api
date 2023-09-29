c9b842fda1bf4c4468bd8781f8a4928845650d34
gosekose <ksyn1611@naver.com>
2023-09-29 16:13:37 +0900

    [Feat] DPL-035: 유저 머문 가게 및 가게 등록 테스트

---
+++ adapter/src/main/kotlin/com/user/adapter/client/NaverSearchClient.kt
@@ -0,0 +1,18 @@
+package com.user.adapter.client
+
+import org.springframework.stereotype.Component
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.Query
+
+@Component
+interface NaverSearchClient {
+
+    @Headers(ApiClientUtil.CONTENT_TYPE_APPLICATION_JSON)
+    @GET("/v1/search/local.json")
+    fun searchLocation(
+        @Query("query") coords: String,
+    ): Call<String>
+
+}
---
+++ adapter/src/test/kotlin/com/user/adapter/location/NaverLocationSearchAdapterTest.kt
@@ -0,0 +1,15 @@
+package com.user.adapter.location
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
+
+class NaverLocationSearchAdapterTest : FunSpec({
+
+
+    context("") {
+        test("") {
+
+        }
+    }
+
+})
