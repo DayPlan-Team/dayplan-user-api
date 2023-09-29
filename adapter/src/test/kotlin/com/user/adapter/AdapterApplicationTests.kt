1065796dda5424b45c6ab55717aac908da64fb76
gosekose <ksyn1611@naver.com>
2023-09-29 16:32:00 +0900

    [Test] DPL-036: 유저 머문 가게 및 가게 등록 테스트 기초 세팅

--- adapter/src/test/kotlin/com/user/adapter/AdapterApplicationTests.kt
+++ adapter/src/test/kotlin/com/user/adapter/AdapterApplicationTests.kt
@@ -2,7 +2,6 @@ package com.user.adapter
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
-import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest(classes = [AdapterTestConfiguration::class])
 class AdapterApplicationTests {
--- adapter/src/test/kotlin/com/user/adapter/AdapterTestConfiguration.kt
+++ adapter/src/test/kotlin/com/user/adapter/AdapterTestConfiguration.kt
@@ -2,9 +2,14 @@ package com.user.adapter
 
 import org.springframework.boot.SpringBootConfiguration
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.domain.EntityScan
+import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import org.springframework.test.context.ActiveProfiles
 
-@Configuration
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = ["com.user.**"])
+@EnableJpaRepositories(basePackages = ["com.user.adapter.**"])
+@EntityScan(basePackages = ["com.user.**"])
+@ComponentScan(basePackages = ["com.user.**"])
 class AdapterTestConfiguration
